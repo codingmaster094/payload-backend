bae3c90525a0450367cc03187448af3542ab03c3
dipak-trio-spire <dipak@trio-spire.com>
2025-06-13 16:35:32 +0530

    add media

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,15 +1,11 @@
 import type { CollectionConfig } from 'payload'
-import path from 'path'
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
   },
-  upload: {
-    staticDir: path.resolve(__dirname, '../../media'),
-    staticURL: '/media',
-  },
+
   fields: [
     {
       name: 'alt',
@@ -17,4 +13,5 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
+  upload: true,
 }
