098a81faf6e59e787a64ebd60daa69d937c74bb2
dipak-trio-spire <dipak@trio-spire.com>
2025-06-16 13:50:16 +0530

    changes Media.ts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+  access: {
+    read: () => true, // ✅ Allow public read access to media files
+  },
   upload: {
     staticDir: 'media',
     imageSizes: [
@@ -20,10 +23,6 @@ export const Media: CollectionConfig = {
       {
         name: 'tablet',
         width: 1024,
-        // By specifying `undefined` or leaving a height undefined,
-        // the image will be sized to a certain width,
-        // but it will retain its original aspect ratio
-        // and calculate a height automatically.
         height: undefined,
         position: 'centre',
       },
